57f2060ae03a95fc0582b3954a38fffc83ff535d
Yii.Guxing <yii.guxing@gmail.com>
2018-11-25 02:06:26 +0800

    Update toolbar UI

---
+++ src/main/kotlin/cn/yiiguxing/tool/dcmviewer/OpRadioButton.kt
@@ -0,0 +1,26 @@
+package cn.yiiguxing.tool.dcmviewer
+
+import javafx.scene.AccessibleAttribute
+import javafx.scene.control.ToggleButton
+
+/**
+ * OpRadioButton
+ *
+ * Created by Yii.Guxing on 2018/11/25
+ */
+class OpRadioButton : ToggleButton() {
+
+    override fun fire() {
+        if (toggleGroup == null || !isSelected) {
+            super.fire()
+        }
+    }
+
+    override fun queryAccessibleAttribute(attribute: AccessibleAttribute, vararg parameters: Any): Any {
+        return when (attribute) {
+            AccessibleAttribute.SELECTED -> isSelected
+            else -> super.queryAccessibleAttribute(attribute, *parameters)
+        }
+    }
+
+}
