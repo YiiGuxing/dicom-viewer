4a3b23603687592725bc684e4bbc83a029182a87
Yii.Guxing <yii.guxing@gmail.com>
2018-11-26 11:35:38 +0800

    Fix non-gray image

--- src/main/kotlin/cn/yiiguxing/tool/dcmviewer/image/DicomImage.kt
+++ src/main/kotlin/cn/yiiguxing/tool/dcmviewer/image/DicomImage.kt
@@ -8,13 +8,12 @@ import javafx.beans.property.BooleanProperty
 import javafx.beans.property.ReadOnlyFloatProperty
 import javafx.beans.property.ReadOnlyFloatWrapper
 import javafx.beans.property.SimpleBooleanProperty
+import javafx.embed.swing.SwingFXUtils
 import javafx.scene.canvas.GraphicsContext
-import javafx.scene.image.PixelFormat
 import javafx.scene.image.WritableImage
 import org.dcm4che3.data.Tag
 import org.dcm4che3.imageio.plugins.dcm.DicomMetaData
-import java.awt.Graphics2D
-import java.awt.geom.AffineTransform
+import java.awt.image.BufferedImage
 import java.awt.image.ColorModel
 import java.awt.image.WritableRaster
 
@@ -71,34 +70,17 @@ class DicomImage(
     }
 
     private fun createDisplayImage(ww: Float, wc: Float): WritableImage {
-        val width = width
-        val height = height
-        val buffer = buffer
         val colorModel = colorModel
         val sourceRaster = sourceRaster
         val image = WritableImage(width, height)
-        val pixelWriter = image.pixelWriter
-
         if (colorModel.numComponents == 3) {
-            val bufferHeight = buffer.size / width
-            val pixelFormat = PixelFormat.getIntArgbInstance()
-            for (h in 0 until height step bufferHeight) {
-                val bh = minOf(height - h, bufferHeight)
-                for (bHeight in 0 until bh) {
-                    var i = 0
-                    val cHeight = h + bHeight
-                    for (w in 0 until width) {
-                        buffer[i++] = colorModel.getRGB(sourceRaster.getSample(w, cHeight, 0))
-                    }
-                }
-
-                pixelWriter.setPixels(0, h, width, bh, pixelFormat, buffer, 0, width)
-            }
+            val bufferedImage = BufferedImage(colorModel, sourceRaster, false, null)
+            SwingFXUtils.toFXImage(bufferedImage, image)
         } else {
             processor.run {
                 windowWidth = ww
                 windowCenter = wc
-                process(sourceRaster, pixelWriter, frame, buffer)
+                process(sourceRaster, image.pixelWriter, frame, buffer)
             }
         }
 
@@ -136,6 +118,10 @@ class DicomImage(
         }
 
         changed = false
+
+        if (colorModel.numComponents == 3) {
+            return
+        }
 
         val processor = processor
         processor.windowWidth = windowWidth
